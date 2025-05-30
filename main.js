2f9fd530403d1935cdb027866d0991baaee3ed36
John M <entity.john@gmail.com>
2025-05-29 21:56:33 -0500

    progress on volarak, unsure best way to adjust

--- main.js
+++ main.js
@@ -4,25 +4,82 @@ let currentGeneral = 'ullar'
 
 function getNextGeneral(currentGeneral) {
   return {
+    ['thormun']: 'jandar',
+    ['jandar']: 'valkrill',
+    ['valkrill']: 'vydar',
     ['vydar']: 'utgar',
     ['utgar']: 'ullar',
-    ['ullar']: 'vydar'
+    ['ullar']: 'thormun',
   }[currentGeneral]
 }
 
 function playNextAnimation(nextGeneral) {
+  //  THORMUN GETS SCALED-Y DOWN
+  if (nextGeneral === 'thormun') {
+    gsap.timeline()
+      .to(MorphSVGPlugin.convertToPath("#morphTarget"), {
+        duration: 1,
+        morphSVG: {
+          shape: "#thormun-logo",
+        },
+      }, 0)
+      .to("#morphTarget", {
+        duration: 0.1,
+        fill: "#cf9942",
+      }, 0)
+      .to("#morphTarget", {
+        duration: 1,
+        scaleX: 1,
+        scaleY: 0.85,
+      }, 0)
+  }
+  //  VALKRILL GETS SCALED DOWN
+  if (nextGeneral === 'valkrill') {
+    gsap.timeline()
+      .to(MorphSVGPlugin.convertToPath("#morphTarget"), {
+        duration: 1,
+        morphSVG: {
+          shape: "#valkrill-logo",
+        },
+      }, 0)
+      .to("#morphTarget", {
+        duration: 0.1,
+        fill: "#cf9942",
+      }, 0)
+      .to("#morphTarget", {
+        duration: 0.65,
+        scale: 0.21,
+      }, 0)
+  }
+  if (nextGeneral === 'jandar') {
+    gsap.timeline()
+      .to(MorphSVGPlugin.convertToPath("#morphTarget"), {
+        duration: 1,
+        morphSVG: {
+          shape: "#jandar-logo",
+        },
+      }, 0)
+      .to("#morphTarget", {
+        duration: 0.1,
+        fill: "#5279b2",
+      }, 0)
+      .to("#morphTarget", {
+        duration: 1,
+        scale: 1,
+      }, 0)
+  }
   if (nextGeneral === 'ullar') {
-    // toUtgar.time(0)
-    // iconTransform.tweenFromTo(1, 2)
-    gsap.timeline().to("#morphTarget", {
-      duration: 1,
-      fill: "green",
-    }, 0).to(MorphSVGPlugin.convertToPath("#morphTarget"), {
-      duration: 1,
-      morphSVG: {
-        shape: "#ullar-logo",
-      },
-    }, 0)
+    gsap.timeline()
+      .to(MorphSVGPlugin.convertToPath("#morphTarget"), {
+        duration: 1,
+        morphSVG: {
+          shape: "#ullar-logo",
+        },
+      }, 0)
+      .to("#morphTarget", {
+        duration: 0.1,
+        fill: "#306a3a",
+      }, 0)
       .to("#morphTarget", {
         duration: 1,
         scale: 1,
@@ -37,14 +94,15 @@ function playNextAnimation(nextGeneral) {
         },
       }, 0)
       .to("#morphTarget", {
-        duration: 1,
-        fill: "red",
+        duration: 0.1,
+        fill: "#81272e",
       }, 0)
       .to("#morphTarget", {
         duration: 1,
         scale: 1,
       }, 0)
   }
+  //  VYDAR GETS SCALED DOWN
   if (nextGeneral === 'vydar') {
     gsap.timeline()
       .to(MorphSVGPlugin.convertToPath("#morphTarget"), {
@@ -54,12 +112,13 @@ function playNextAnimation(nextGeneral) {
         },
       }, 0)
       .to("#morphTarget", {
-        duration: 1,
+        duration: 0.1,
         fill: "#8598a8",
       }, 0)
       .to("#morphTarget", {
         duration: 1,
-        scale: 0.7,
+        scaleY: 0.7,
+        scaleX: 0.8,
       }, 0)
   }
 }
